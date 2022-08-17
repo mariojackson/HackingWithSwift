e304037daae82605744315d011e3857672d6da7e
Mario Jackson <mario@jackson.softwre>
2022-08-17 21:53:32 +0200

    Initial sub project setup: Names to Faces

---
+++ NamesToFaces/NamesToFaces/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Mario Jackson on 8/17/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
---
+++ NamesToFaces/NamesToFaces/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  NamesToFaces
+//
+//  Created by Mario Jackson on 8/17/22.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue a Person cell.")
+        }
+        return cell
+    }
+}
+
