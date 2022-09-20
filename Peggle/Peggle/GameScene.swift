ad5f105935e7aefb9072eab2d7ffc4c98d5f872a
Mario Jackson <mario@jackson.softwre>
2022-09-20 18:04:11 +0200

    Create slots and make them spin

--- Peggle/Peggle/GameScene.swift
+++ Peggle/Peggle/GameScene.swift
@@ -23,6 +23,11 @@ class GameScene: SKScene {
         makeBouncer(at: CGPoint(x: 512, y: 0))
         makeBouncer(at: CGPoint(x: 768, y: 0))
         makeBouncer(at: CGPoint(x: 1024, y: 0))
+        
+        makeSlot(at: CGPoint(x: 128, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 384, y: 0), isGood: false)
+        makeSlot(at: CGPoint(x: 640, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 896, y: 0), isGood: false)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -43,5 +48,26 @@ class GameScene: SKScene {
         bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2)
         bouncer.physicsBody?.isDynamic = false
         addChild(bouncer)
+    }
+    
+    func makeSlot(at position: CGPoint, isGood: Bool) {
+        var slotBase: SKSpriteNode
+        var slotGlow: SKSpriteNode
+        
+        let imageNameSlotBase = isGood ? "slotBaseGood" : "slotBaseBad"
+        let imageNameSlotGlow = isGood ? "slotGlowGood" : "slotGlowBad"
+        
+        slotBase = SKSpriteNode(imageNamed: imageNameSlotBase)
+        slotGlow = SKSpriteNode(imageNamed: imageNameSlotGlow)
+        
+        slotBase.position = position
+        slotGlow.position = position
+        
+        addChild(slotBase)
+        addChild(slotGlow)
+        
+        let spin = SKAction.rotate(byAngle: .pi, duration: 10)
+        let spinForever = SKAction.repeatForever(spin)
+        slotGlow.run(spinForever)
     }
 }
